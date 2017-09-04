399d7c88a9a4fdd13d4f806bc4f3827193958229
TeaAmmunet <tea.ammunet@gmail.com>
2017-09-04 16:16:49 +0200

    Testing to download packages and playing around w ecoli

---
+++ testcode.R
@@ -0,0 +1,21 @@
+library(seqinr)
+source("http://bioconductor.org/biocLite.R")
+biocLite()
+library(BiocInstaller)
+biocVersion()
+biocValid()
+biocLite(c("GenomicRanges","biomaRt", "AnnotationDbi" ))
+biocLite("Biostrings")
+biocLite("BSgenome.Ecoli.NCBI.20080805")
+library(GenomicRanges)
+library(biomaRt)
+library(BSgenome.Ecoli.NCBI.20080805)
+#help("useMart")
+#mart=useMart("ensembl")
+#listDatasets(mart)
+#Importing ecoli genome to test
+ecl<-Ecoli$NC_008563
+#counting nucleotides (this one counts AAs as well)
+alphabetFrequency(ecl)
+#counting only nucleotides
+letterFrequency(ecl, letters = "ACGT", OR=0)
