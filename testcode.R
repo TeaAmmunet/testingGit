e9f185774ffef48d601a76d741adedc41114b583
TeaAmmunet <tea.ammunet@gmail.com>
2017-09-08 10:48:33 +0200

    Working cumGC plot with window and step.

--- testcode.R
+++ testcode.R
@@ -32,15 +32,16 @@ plot(1:length(gc),gc)
 lines(lowess(x = 1:length(gc), y= gc, f = 0.10), col = 12, lwd = 2)
 #Cumulative GC skew curve
 ##First to create the vector y and x (x = length of sequence)
-y<- list()
+
 sq<-as.character(eclDna)
-eclDNA[seq]
-ecl
-sq[[1]]
+#eclDNA[seq]
+#ecl
+#sq[[1]]
 splitsq<-strsplit(sq,"")[[1]]
 splitsq[2]=="A"
+y<- vector(,length=length(splitsq))
 #trying first on shorter seq
-for (i in 1:20){
+for (i in 1:length(splitsq)){
   if (splitsq[i] == "G"){
   y[i]=1
   } else if (splitsq[i] =="C"){
@@ -48,6 +49,30 @@ for (i in 1:20){
   } else
     y[i]=0
 }
-y[1:20]
-y[[2]][1]
-y2[2]
+#length(y)
+cumy<-vector()
+#cumy[1]+y2[2]
+#y2[2][1]cumy[2]<-cumy[1]+y2[2]
+#instead of whole genome, using overlapping windows
+#wind<-window(y,width=1000) #does not work, meant for time series?
+#lengths(wind)
+wind<-1000
+step<-100
+windylen<- ceiling((length(y)-window)/step)
+windy<-vector(,length=windylen)
+start<-1
+for (j in 1:windylen){
+  windy[j]<-sum(y[start:(start+wind)])
+    start<-start+step
+}
+#windy
+cumy<-cumsum(windy)
+#x<-vector(,length=length(splitsq))
+#x<-rep(1:length(splitsq))
+#plot(x,cumy)
+windx<-rep(1:windylen)
+plot(windx,cumy, type='l')
+#lines(windx,cumy)
+#length(x)
+#length(cumy)
+#length(splitsq)
