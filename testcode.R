4a27c9f6203e3fdaece7cf09d6843f1dc631e9b8
TeaAmmunet <tea.ammunet@gmail.com>
2017-09-17 10:42:57 +0200

    FAsta input and output for progidal plus manipulations to get 3rd codon position

--- testcode.R
+++ testcode.R
@@ -59,7 +59,9 @@ cumy<-vector()
 #lengths(wind)
 wind<-1000
 step<-100
-windylen<- ceiling((length(y)-window)/step)
+windylen<- ceiling((length(y)-window)/step)#think of changing so that all pieces the same size
+#, and last piece overlaps with the beginning
+#also check x axis
 windy<-vector(,length=windylen)
 start<-1
 for (j in 1:windylen){
@@ -93,20 +95,143 @@ methods(getAnnot)
 eclDNAseq<-readBStringSet("eclFasta.fasta", format='fasta')
 eclDNAseq<-gsub(" ", "", eclDNAseq, fixed = TRUE)
 eclDNAseq['seq']
-library(micropan)
-#extdata.path <- file.path(path.package("micropan"),"extdata")
-prodigalPredict("eclFasta.fasta","eclPredGen.fasta")
-#echo %PATH%
 system("prodigal -h")
+###------------------Prodigal and GC3 things begin here-------------------
 #From the results, we need to 1. read.fasta 
 #2. chage it into string and split it 
 #3. get the start + stop 
 #4. change start and stop into float
-eclGen<-read.fasta(file="eclPredGen.fasta")
-#attributes(eclGen[3])$Annot
-eclAn3<-strsplit(as.character(getAnnot(eclGen[3]))," ")
-eclAn3[[1]][3]
-eclAn3_start<-as.numeric(eclAn3[[1]][3])
-eclAn3_stop<-as.numeric(eclAn3[[1]][5])
-eclAn3_len<-(as.numeric(eclAn3[[1]][5])-as.numeric(eclAn3[[1]][3]))
-eclAn3_len
+#eclGen<-read.fasta(file="eclPredGen.fasta")
+#Calling prodigal directly without micropan
+system ('prodigal -h')
+system('prodigal -i eclFasta.fasta -o eclGenCoord.fasta -d eclGenSeqs.fasta')
+#genfile<-system.file('eclPredgen.gbk', package="genbankr")
+#Getting genbankr package for parsing gbk files
+#biocLite("genbankr")
+#library(genbankr)
+#gb<-readGenBank(genfile)
+#testgenb=readGenBank(system.file('sample.gbk',package='genbankr'))#Doesnt work
+#Trying parsing with regular delim import
+#delims<-c('\t',';','.')
+#eclGFF<-read.delim('eclGenCoord.gff', header=TRUE,sep='\t', skip=2)
+#head(eclGFF)
+#eclGFF[[1]][3]
+#firstg<-eclGFF[[1]][1]
+#charg<-as.character(firstg)
+#charg
+#-> hard to extract gene start and stop
+#Trying with GenFeatures instead
+#library(GenomicFeatures)
+#eclgenabank<-readGenBank(system.file("eclPredgen.gbk", package='genbankr'))
+#ecltxdb<-makeTxDbFromGenBank('eclPredgen.gbk')#complicated does not work
+#--------THIS IS THE CURRENT WAY-------------------
+#trying with fasta again
+#eclgenfasta<-read.delim('eclPredGen.fasta', sep='', fill=TRUE)
+#head(eclgenfasta)
+#eclgenfasta #not easy from here either...weird Xs before numbers
+eclgenfasta2<-read.fasta('eclPredGen.fasta')
+eclgenseq<-read.fasta('eclGenSeqs.fasta')
+head(eclgenfasta2)
+head(eclgenseq)
+getAnnot(eclgenfasta2)
+typeof(eclgenseq)
+#getSequence.SeqFastadna(eclgenfasta2)
+eclannot<-sapply(eclgenfasta2,function(x) getAnnot(x))
+#Try9ing to get 3rd codon positions with one gene first
+oneseq<-getSequence(eclgenseq[[1]])
+thirdstry<-oneseq[seq(1,length(oneseq),3)]
+thirdstry
+oneseq[1]
+typeof(eclgenseq)
+#in principle, for every element (gene) in eclgenseq, take every third element to a new vector
+#outhird<-vector(mode='character')
+threes<-function(mydata){
+  dna<-getSequence(mydata)
+  #print('getSeq works')
+  everythird<-dna[seq(1,length(dna),3)]
+  #append(outhird,everythird, after=length(outhird))
+  #outhird
+  as.vector(everythird)
+}
+eclgc3s<-lapply(eclgenseq,FUN=threes)
+#headgc3<-head(lapply(eclgenseq,FUN=threes))
+head(eclgc3s)
+#headgc3
+#Trying to get rid of names, does not work
+#library(rlang)
+#flatgc3<-flatten(eclgc3s)
+#flatter<-flatten(flatgc3)
+#remove(flatter)
+#remove(flatgc3)
+#head(flatter)
+#length(eclgc3s[1:2])
+#input= list with headings
+gc3<-function(mycodons){
+  outgc3<-vector('list')
+  for (j in 1:length(mycodons)){
+    for (i in 1:length(mycodons[[j]])){
+      if (mycodons[[j]][i] == "g"){
+        c(outgc3,1)
+      } else if (mycodons[[j]][i] =="c"){
+        c(outgc3,(-1))
+      } else{
+        c(outgc3,0)
+      }
+    }
+  }
+  outgc3
+}
+gc3(eclgc3s[1:2])
+head(gc3y)
+gc3(eclgc3s[1:2])
+#------------old prodigal fasta manipulations from here down------------------
+eclannot[[1]]
+typeof(eclannot)
+eclsplit<-sapply(eclannot, function(x) strsplit(x,"#"))
+eclsplit[[1]][2:3]
+head(eclsplit)
+eclcoord<-lapply(lapply(eclsplit, '[', 1:4),function(x) as.integer(x[2:4]))#taking the start and the stop and converting them into integers
+head(eclcoord)
+typeof(eclcoord)
+eclgenes<-data.frame(t(sapply(eclcoord,c)))
+eclgenes[[1]][2]
+colnames(eclgenes)<-c('start','stop', 'direction')
+head(eclgenes)
+eclgenes$len=(eclgenes$stop-eclgenes$start)
+#Getting codons from one strand
+#calculating codons
+#codons within a gene
+c<-vector(mode='numeric')
+dim(eclgenes)[1]
+tail(eclgenes)
+length(eclgenes[['start']])
+#testfu<-function(mydata){
+#  for (i in 1:dim(mydata)[1]){
+#    c[i]=i
+#  }
+#return(c)
+#}
+#testfu(eclgenes)
+(eclgenes$len[1]+1)/3
+eclcods<-vector(mode='numeric')
+a<-c(1,2,3)
+eclcods[1]<-0
+append(eclcods,a,after=length(eclcods))
+codstart[1]<-eclgenes[['start']][1]
+codfun<-function(mydata){
+  if(mydata[['direction']]==1){
+    print('+1 strand')
+    nrcodons<-(mydata[['len']]+1)/3
+    for (i in 2:nrcodons){
+      codstart[i]<-codstart[i-1]+3
+    }
+    print('for loop ok')
+  }
+  append(eclcods,codstart, after=length(eclcods))
+  #return(eclcods)
+  codstart
+  eclcods
+}
+codfun(eclgenes[1,])
+eclgencod<-apply(eclgenes,1,FUN=codfun) #General looping works, but output is empty, and does not return codstart
+head(codstart)
